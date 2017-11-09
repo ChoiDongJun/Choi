64f0598f425f7ae7009f6b622f77f2fe527ea6e7
java-04 <java-04@java-04-PC>
2017-11-09 15:51:22 +0900

    add dao method complete!!!

--- board/src/com/board/impl/BoardDAOImpl.java
+++ board/src/com/board/impl/BoardDAOImpl.java
@@ -1,7 +1,17 @@
 package com.board.impl;
 
+import java.sql.SQLException;
+
 import com.board.DAO.BoardDAO;
 
 public class BoardDAOImpl implements BoardDAO {
+
+	@Override
+	public void insertBoard() throws SQLException {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
 
 }
