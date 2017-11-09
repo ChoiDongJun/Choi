84119a603601d0c469ea845e97d1bd05ee3b5b7e
java-04 <java-04@java-04-PC>
2017-11-09 16:16:41 +0900

    modify dao insert() add dao delete()

--- board/src/com/board/DAO/BoardDAO.java
+++ board/src/com/board/DAO/BoardDAO.java
@@ -3,5 +3,7 @@ package com.board.DAO;
 import java.sql.SQLException;
 
 public interface BoardDAO {
-   void insert(String board) throws SQLException;
+   void insert(int board) throws SQLException;
+   
+   void delete(String board) throws SQLException;
 }
